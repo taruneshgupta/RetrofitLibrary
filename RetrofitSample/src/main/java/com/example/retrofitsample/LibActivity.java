a289b713ea9367c3e17eaf89919be28ddf6b7cb8
taruneshgupta <tarunesh.gupta@eko.co.in>
2021-09-19 20:31:14 +0530

    Initial commit

---
+++ RetrofitSample/src/main/java/com/example/retrofitsample/LibActivity.java
@@ -0,0 +1,14 @@
+package com.example.retrofitsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lib);
+    }
+}
